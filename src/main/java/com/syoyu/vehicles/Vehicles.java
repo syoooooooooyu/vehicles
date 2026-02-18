021e41a78b6321dc8e52e64ee28ef7d844b9538f
syoooooooooyu <ryasyo1224@gmail.com>
2026-02-18 15:47:22 +0900

    Initial commit: 1.20.10 Mod

---
+++ src/main/java/com/syoyu/vehicles/Vehicles.java
@@ -0,0 +1,32 @@
+package com.syoyu.vehicles;
+
+import com.mojang.logging.LogUtils;
+import com.syoyu.vehicles.event.EventRegister;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Vehicles.MOD_ID)
+public final class Vehicles {
+
+    public static final String MOD_ID = "vehicles";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    private static Vehicles instance = null;
+
+    public Vehicles(FMLJavaModLoadingContext context) {
+        instance = this;
+        EventRegister.register(context.getModBusGroup());
+    }
+
+    public static Vehicles getInstance(){
+        if(instance == null){
+            throw new IllegalStateException("Vehicles instance is not initialized yet!");
+        }
+        return instance;
+    }
+
+    public static Logger getLogger(){
+        return LOGGER;
+    }
+}
---
+++ src/main/java/com/syoyu/vehicles/event/EventRegister.java
@@ -0,0 +1,10 @@
+package com.syoyu.vehicles.event;
+
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+
+public final class EventRegister {
+
+    public static void register(BusGroup bus){
+
+    }
+}
